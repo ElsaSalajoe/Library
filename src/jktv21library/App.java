0c8672c0ace7155539717c3bc24cb3dad44cd845
pupil <pupil@ivkhk.ee>
2022-10-10 11:19:13 +0300

    Добавление классов

--- src/jktv21library/App.java
+++ src/jktv21library/App.java
@@ -1,5 +1,8 @@
 package jktv21library;
 
+import entity.Book;
+import entity.Reader;
+import entity.Status;
 import java.util.Scanner;
 
     public class App {
@@ -9,6 +12,14 @@ import java.util.Scanner;
             do{
                 System.out.println("Функции приложения: ");
                 System.out.println("0. Закрыть приложение");
+                System.out.println("1. Добавить книгу");
+                System.out.println("2. Добавить посетителей");
+                System.out.println("3. История выдачи книги на руки");
+                System.out.println("4. История возврата книги");
+                System.out.println("5. Текущий статус книги");
+                System.out.println("6. Список книг");
+                System.out.println("7. Список авторов");
+                
                 int task = sc.nextInt();
                 System.out.println("Выбери номер функции: ");
                 sc.nextLine();
@@ -16,11 +27,38 @@ import java.util.Scanner;
                     case 0:
                         repeat = false;
                         break;
+                    case 1:
+                        System.out.println("1. Добавить книгу");
+                        Book book = new Book();
+                        
+                        break;
+                    case 2:
+                        System.out.println("2. Добавить посетителей");
+                        Reader reader = new Reader();
+                        break;
+                    case 3:
+                        System.out.println("3. История выдачи книги на руки");
+                        
+                        break;
+                    case 4:
+                        System.out.println("4. История возврата книги");
+                        
+                        break;
+                    case 5:
+                        System.out.println("5. Текущий статус книги");
+                        Status status = new Status();
+                        break;
+                    case 6:
+                        System.out.println("6. Список книг");
+                        break;
+                    case 7:
+                        System.out.println("7. Список авторов");
+                        break;
                     default:
                         System.out.println("Выбери номер функции из списка!");
             }
             }while(repeat);
-            System.out.println("Пока!");
+            System.out.println("До свидания!");
         }
     
 }
